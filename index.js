350b04eabac0c18eef3fe0c974e9ced4244c24b2
emeraldhycient <igwezehycient86@gmail.com>
2025-02-15 18:09:05 +0200

    feat: added sample string that contains all the requirements of the assigment

---
+++ index.js
@@ -0,0 +1,10 @@
+// Sample input string containing various data types
+const inputString = `
+  Contact us at support@alueducation.com or visit https://www.alueducation.com for more info.
+  Call us at (123) 456-7890 or 123-456-7890.
+  Here's some HTML: <p>Hello World</p> and <div class="example">Content</div>.
+  Payment can be made using 1234 5678 9012 3456 or 1234-5678-9012-3456.
+  Follow us on #example and #ThisIsAHashtag.
+  Prices start at $19.99 and go up to $1,234.56.
+  The meeting is at 14:30 or 2:30 PM.
+`;
